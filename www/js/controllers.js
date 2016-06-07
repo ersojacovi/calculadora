687edce7e58291c9509258fee1d6fcf460ec6167
ersojacovi <ersojacovi@gmail.com>
2016-06-07 19:52:11 +0200

    English translation, delete unused files

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,143 +2,140 @@ angular.module('starter.controllers', [])
 
 .controller('DashCtrl', function($scope) {
 	
-	var numpantalla="0"; //guardar número en pantalla
-	var iniciopantalla=1; //iniciar número en pantalla: 1=si; 0=no;
-	var coma=0; //estado coma decimal 0=no, 1=si;
-	var noculto=0; //número oculto o en espera.
-	var op="no"; //operación en curso; "no" =  sin operación.
+	var numpantalla="0"; //Save screen number
+	var iniciopantalla=1; //Reinicialize screen number: 1=yes; 0=no;
+	var coma=0; //Decimal point 0=no, 1=yes;
+	var noculto=0; //Hidden number or waiting.
+	var op="no"; //Doing an operation; "no" =  no operation.
 	
 	$scope.numeroactual="0";
 	
-	$scope.numero = function(valor) {  //recoge el número pulsado en el argumento.
+	// -------------------------------------------------------------------------------
+	$scope.numero = function(valor) {  //Get number clicked in the argument.
 	   console.log(valor+"-"+numpantalla+"-"+iniciopantalla+"-"+coma);
 	   
-	   if (numpantalla=="0" || iniciopantalla==1  ) { // inicializar un número, 
-       		$scope.numeroactual=valor; //mostrar en pantalla
-            numpantalla=valor; //guardar número;
-            if (valor==".") { //si escribimos una coma al principio del número
-               $scope.numeroactual="0."; //escribimos 0.
-               numpantalla=valor; //guardar número
-               coma=1; //cambiar estado de la coma
+	   if (numpantalla=="0" || iniciopantalla==1  ) { // Inicialize number, 
+       		$scope.numeroactual=valor; //Show screen
+            numpantalla=valor; //Save number;
+            if (valor==".") { //If a decimal point is clicked as first value
+               $scope.numeroactual="0."; //Write 0.
+               numpantalla=valor; //Save number
+               coma=1; //Change point state
                }
       	}
-       	else { //continuar escribiendo un número
-               if (valor=="." && coma==0) { //si escribimos una coma decimal pòr primera vez
+       	else { //Continue writting number
+               if (valor=="." && coma==0) { //If a decimal point is clicked for first time
                    $scope.numeroactual+=valor;
                    numpantalla+=valor;
-                   coma=1; //cambiar el estado de la coma  
+                   coma=1; //Change point state  
                }
-              //si intentamos escribir una segunda coma decimal no realiza ninguna acción.
+              //If we try to put a second point, it does nothing.
                else if (valor=="." && coma==1) {} 
-               //Resto de casos: escribir un número del 0 al 9: 	 
+               //Rest of choices: we write a number from 0 to 9: 	 
                else {
                    $scope.numeroactual+=valor;
                    numpantalla+=valor
                }
        }
-       iniciopantalla=0 //el número está iniciado y podemos ampliarlo.
+       iniciopantalla=0 //The number is inicialized and it can be enlarged.
 	};
 	
+	// -------------------------------------------------------------------------------
+
 	$scope.operar =function (operacion) {
-		 $scope.igualar(); //si hay operaciones pendientes se realizan primero
-         noculto=numpantalla; //ponemos el 1º número en "numero en espera" para poder escribir el segundo.
-         op=operacion; //guardamos tipo de operación.
-         iniciopantalla=1; //inicializar pantalla.
+		 $scope.igualar(); //If there are operations to do, then do it now
+         noculto=numpantalla; //Put the first number in "waiting" to write the second one.
+         op=operacion; //Save the type of operation.
+         iniciopantalla=1; //Initialize screen.
     };
 	
+	// -------------------------------------------------------------------------------
+
 	$scope.igualar= function() {
-         if (op=="no") { //no hay ninguna operación pendiente.
-            $scope.numeroactual=numpantalla;	//mostramos el mismo número	
+         if (op=="no") { //No operation to do.
+            $scope.numeroactual=numpantalla;	//Show the same number	
             }
          else { //con operación pendiente resolvemos
-            var sl=noculto+op+numpantalla; // escribimos la operación en una cadena
-            var sol=eval(sl) //convertimos la cadena a código y resolvemos
-            $scope.numeroactual=sol //mostramos la solución
-            numpantalla=sol; //guardamos la solución
-            op="no"; //ya no hay operaciones pendientes
-            iniciopantalla=1; //se puede reiniciar la pantalla.
+            var sl=noculto+op+numpantalla; // Write the operation in an array
+            var sol=eval(sl) //Convert the array to code and evaluate it
+            $scope.numeroactual=sol //Show answer
+            numpantalla=sol; //Save answer
+            op="no"; //No opertations to do
+            iniciopantalla=1; //Reinitialize screen.
             }
     };
 	
+	// -------------------------------------------------------------------------------
+	
 	$scope.raizc=function() {
-         numpantalla=Math.sqrt(numpantalla) //resolver raíz cuadrada.
-         $scope.numeroactual=numpantalla; //mostrar en pantalla resultado
-         op="no"; //quitar operaciones pendientes.
-         iniciopantalla=1; //se puede reiniciar la pantalla 
+         numpantalla=Math.sqrt(numpantalla) //Resolve Square root.
+         $scope.numeroactual=numpantalla; //Show result
+         op="no"; //No operations to do.
+         iniciopantalla=1; //Reinitialize screen. 
     };
+	
+	// -------------------------------------------------------------------------------
 	
 	$scope.porcent=function() { 
-         numpantalla=numpantalla/100; //dividir por 100 el número
-         $scope.numeroactual=numpantalla; //mostrar en pantalla
-         $scope.igualar(); //resolver y mostrar operaciones pendientes
-         iniciopantalla=1; //reiniciar la pantalla
+         numpantalla=numpantalla/100; //divide the number by 100
+         $scope.numeroactual=numpantalla; //Show screen
+         $scope.igualar(); //resolve and show pending operations
+         iniciopantalla=1; //Reinitialize lscreen
     };
+	
+	// -------------------------------------------------------------------------------
 	
 	$scope.opuest=function() { 
-		console.log(numpantalla);
-		var nx=Number(numpantalla); //convertir en número
+		//console.log(numpantalla);
+		var nx=Number(numpantalla); //Convert the number
 		console.log(nx);
-		nx=-nx; //cambiar de signo
+		nx=-nx; //Change the sign
 		console.log(nx);
-		numpantalla=String(nx); //volver a convertir a cadena
-		$scope.numeroactual=numpantalla; //mostrar en pantalla.
-		console.log(numpantalla);
+		numpantalla=String(nx); //Convert to array again
+		$scope.numeroactual=numpantalla; //Show screen.
+		//console.log(numpantalla);
     };
 	
+	// -------------------------------------------------------------------------------
+
 	$scope.inve=function() {
          var nx=Number(numpantalla);
          nx=(1/nx);
          numpantalla=String(nx);		 
          $scope.numeroactual=numpantalla;
-         iniciopantalla=1; //reiniciar pantalla al pulsar otro número.
+         iniciopantalla=1; //Reinitialize screen when click another number.
     };
 	
-	$scope.retro=function(){ //Borrar sólo el último número escrito.
-         var cifras=numpantalla.length; //hayar número de caracteres en pantalla
-         var br=numpantalla.substr(cifras-1,cifras);//info del último caracter
-         numpantalla=numpantalla.substr(0,cifras-1); //quitar el ultimo caracter
-         if (numpantalla=="") {numpantalla="0";} //si ya no quedan caracteres, pondremos el 0
-         if (br==".") {coma=0;} //Si hemos quitado la coma, se permite escribirla de nuevo.
-         $scope.numeroactual=numpantalla; //mostrar resultado en pantalla	 
+	// -------------------------------------------------------------------------------
+
+	$scope.retro=function(){ //Delete only the last number.
+         var cifras=numpantalla.length; //Find number of char on screen
+         var br=numpantalla.substr(cifras-1,cifras);//info of last char
+         numpantalla=numpantalla.substr(0,cifras-1); //Delete the last char
+         if (numpantalla=="") {numpantalla="0";} //If it was the last char, put 0
+         if (br==".") {coma=0;} //If it deleted the decimal point, it can be put it again.
+         $scope.numeroactual=numpantalla; //Show result	 
     };
 	
+	// -------------------------------------------------------------------------------
+
 	$scope.borradoParcial=function() {
-        $scope.numeroactual=0; //Borrado de pantalla;
-        numpantalla=0; //Borrado indicador número pantalla.
-        coma=0; //reiniciamos también la coma					
+        $scope.numeroactual=0; //Screen deleted;
+        numpantalla=0; //Delete screen number.
+        coma=0; //Reinitialize decimal point					
     }
 	
+	// -------------------------------------------------------------------------------
+
 	$scope.borradoTotal=function() {
-         $scope.numeroactual=0; //poner pantalla a 0
-         numpantalla="0"; //reiniciar número en pantalla
-         coma=0; //reiniciar estado coma decimal 
-         noculto=0; //indicador de número oculto a 0;
-         op="no"; //borrar operación en curso.
+         $scope.numeroactual=0; //Put screen to 0
+         numpantalla="0"; //Reinitialize screen number
+         coma=0; //Reinitialize decimal point	
+         noculto=0; //Hidden number to 0;
+         op="no"; //Delete actual operation.
     }
 	
 })
 
-.controller('ChatsCtrl', function($scope, Chats) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
-
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
+.controller('ChatsCtrl', function($scope) {
 })
-
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
-})
-
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
-});
